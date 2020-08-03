35fe9ce36b0bdf5b59c4be3d5492f1287c0bd771
Daniyal Talat <daniyal.fa12bcs@gmail.com>
2020-08-03 12:40:40 +0500

    Fixed bugs in Stripe Implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -16,12 +16,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    //line_items is information about the product, above is information about session
-    line_items: [
+    //display_items is information about the product, above is information about session
+    display_items: [
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
         amount: tour.price * 100, //convert to dollar; 1 dollar = 100 cents
         currency: 'usd',
         quantity: 1,
@@ -40,7 +42,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   //first user is found via its email, then only its ID is read from the result
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
 
   await Booking.create({ tour, user, price });
 };
